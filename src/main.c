8f791cc89f8cbedf0599475ee7e3dbbf163b1919
christopher-besch <mail@chris-besch.com>
2024-01-31 12:34:11 +0100

    added external oscillator

--- src/main.c
+++ src/main.c
@@ -1,46 +1,11 @@
-#include <avr/interrupt.h>
+#include <avr/delay.h>
 #include <avr/io.h>
-#include <avr/sleep.h>
+#include <util/delay.h>
+#include <util/setbaud.h>
 
-int main(void)
+void set_led(uint8_t state)
 {
-    // set DDD2 to output
-    DDRD |= 1 << DD2;
-    // set DDB1 to input
-    DDRB &= ~(1 << DD1);
-    // enable pullup
-    // PUD is not set
-    PORTB |= 1 << PORT1;
-
-    // enable pin change interrupts 0
-    PCICR |= 1 << PCIE0;
-    // enable PORTB1 for pin change interrupt
-    PCMSK0 |= 1 << PCINT1;
-
-    // set PORTD2 to high
-    PORTD |= 1 << PORT2;
-
-    set_sleep_mode(SLEEP_MODE_PWR_DOWN);
-    // interrupt enabled part
-    {
-        sei();
-        // sei does the same as
-        // SREG |= 1 << SREG_I;
-        sleep_enable();
-        while(1) {
-            sleep_cpu();
-        }
-        sleep_disable();
-        cli();
-    }
-}
-
-// button press handler
-// use pin change interrupt 0 on PORTB1
-ISR(PCINT0_vect)
-{
-    // is PINB1 high; so button not pressed?
-    if(PINB & 1 << PIN1) {
+    if(state) {
         // set PORTD2 to high
         PORTD |= 1 << PORT2;
     }
@@ -50,6 +15,61 @@ ISR(PCINT0_vect)
     }
 }
 
-// resulting current draw at 5V:
-// 0.47mA with LED off
-// 9mA with LED on
+void led_init()
+{
+    // set DDD2 to output
+    DDRD |= 1 << DD2;
+    set_led(0);
+}
+
+void uart_trans(unsigned char data)
+{
+    // wait for empty transmit buffer
+    while(!(UCSR0A & (1 << UDRE0)))
+        ;
+
+    // send data
+    UDR0 = data;
+}
+
+void uart_init()
+{
+#define FOSC   2000000 // Clock Speed
+#define BAUD   9600
+#define MYUBRR (FOSC / 16 / BAUD - 1)
+    UBRR0H = (unsigned char)(MYUBRR >> 8);
+    UBRR0L = (unsigned char)MYUBRR;
+
+    // calculated by <util/setbaud.h>
+    // UBRR0H = UBRRH_VALUE;
+    // UBRR0L = UBRRL_VALUE;
+    // enable receiver and transmitter
+    // leave UCSZ02 to 0 for 8 data bits
+    UCSR0B = (1 << RXEN0) | (1 << TXEN0);
+    // set frame format: async, no parity, 1 stop bit, 8 data bits
+    UCSR0C = (1 << USBS0) | (1 << UCSZ01) | (1 << UCSZ00);
+}
+
+int main(void)
+{
+    led_init();
+    uart_init();
+    while(1) {
+        set_led(1);
+        uart_trans('h');
+        uart_trans('e');
+        uart_trans('l');
+        uart_trans('l');
+        uart_trans('o');
+        uart_trans(' ');
+        uart_trans('w');
+        uart_trans('o');
+        uart_trans('r');
+        uart_trans('l');
+        uart_trans('d');
+        uart_trans('\r');
+        uart_trans('\n');
+        set_led(0);
+        _delay_ms(500);
+    }
+}
